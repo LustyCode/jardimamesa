ce8a1287a312e9c9f9a0b4d172749e1a83bb1aec
Lusty Code inc <152020848+LustyCode@users.noreply.github.com>
2023-11-27 22:27:22 -0300

    V1.0

---
+++ assets/function.js
@@ -0,0 +1,70 @@
+// Carrossel
+window.onload = function() {
+
+    var slider2 = new Slider({
+         images: '.slider-2 img',
+         btnPrev: '.slider-2 .buttons .prev',
+         btnNext: '.slider-2 .buttons .next',
+         auto: true,
+         rate: 5000
+     });
+ }
+ 
+ function Slider(obj) {
+ 
+   this.images = document.querySelectorAll(obj.images);
+   this.auto = obj.auto;
+   this.btnPrev = obj.btnPrev;
+   this.btnNext = obj.btnNext;
+     this.rate = obj.rate || 1000;
+ 
+   var i = 0;
+     var slider = this;
+ 
+   this.prev = function () {
+     slider.images[i].classList.remove('shown');
+     i--;
+ 
+     if (i < 0) {
+       i = slider.images.length - 1;
+     }
+ 
+     slider.images[i].classList.add('shown');
+   }
+ 
+   this.next = function () {
+     slider.images[i].classList.remove('shown');
+     i++;
+ 
+     if (i >= slider.images.length) {
+       i = 0;
+     }
+ 
+     slider.images[i].classList.add('shown');
+   }
+ 
+     document.querySelector(slider.btnPrev).onclick = slider.prev;
+     document.querySelector(slider.btnNext).onclick = slider.next;
+ 
+   if (slider.auto)  {
+         setInterval(slider.next, slider.rate);
+     }
+ };
+
+
+// Botão expandir experiência
+var escondido = document.getElementById('experiencia-mostrar');
+
+ function mostrarExp() {
+   if (escondido.classList.contains('d-none')){
+    escondido.classList.remove('d-none');
+   }else{
+    escondido.classList.add('d-none')
+   }
+ }
+
+ function menuOnClick() {
+  document.getElementById("menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change");
+  document.getElementById("menu-bg").classList.toggle("change-bg");
+}
